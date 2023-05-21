4dc854a004786f4e4b1b6d4eca9f46c8cd3b6480
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-21 14:24:03 +0800

    chore(scroll): pass test

--- src/scroll.ts
+++ src/scroll.ts
@@ -4,13 +4,13 @@ export function scroll(key: string, val: string) {
   const [value, important] = transformImportant(val)
 
   if (key.startsWith('scroll-snap'))
-    return `snap-${value}${important}`
+    return `${important}snap-${value}`
   if (key === 'scroll-behavior')
-    return `scroll-${value}${important}`
+    return `${important}scroll-${value}`
   // margin padding
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [_, prefix, suffix] = key.match(/scroll-(margin|padding)-?([\w]+)?/)!
   if (suffix)
-    return `scroll-${prefix[0]}${suffix[0]}-${value}${important}`
-  return `scroll-${prefix[0]}-${value}${important}`
+    return `${important}scroll-${prefix[0]}${suffix[0]}-${value}`
+  return `${important}scroll-${prefix[0]}-${value}`
 }
