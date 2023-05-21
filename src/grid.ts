6b704298c558c558736b5c9b45f08ed29f9277f7
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-21 14:06:47 +0800

    chore(grid): pass test

--- src/grid.ts
+++ src/grid.ts
@@ -12,33 +12,33 @@ export function grid(key: string, val: string) {
   if (key.startsWith('grid-template')) {
     const matcher = value.match(/repeat\s*\(\s*([0-9]+)/)
     if (matcher) {
-      return `grid-${getLastName(key) === 'rows' ? 'rows' : 'cols'}-${
-        matcher[1]
-      }${important}`
+      return `${important}grid-${
+        getLastName(key) === 'rows' ? 'rows' : 'cols'
+      }-${matcher[1]}`
     }
-    return `grid-${
+    return `${important}grid-${
       getLastName(key) === 'rows' ? 'rows' : 'cols'
-    }-${value}${important}`
+    }-${value}`
   }
   if (key === 'grid-auto-flow') {
-    return `grid-flow-${joinWithLine(value).replace(
+    return `${important}grid-flow-${joinWithLine(value).replace(
       'column',
       'col',
-    )}${important}`
+    )}`
   }
   if (key.startsWith('grid-auto')) {
     const matcher = value.match(/minmax\s*\(\s*0\s*,\s*1fr/)
-    return `auto-${getLastName(key) === 'rows' ? 'rows' : 'cols'}-${
+    return `${important}auto-${getLastName(key) === 'rows' ? 'rows' : 'cols'}-${
       matcher ? 'fr' : getFirstName(value)
-    }${important}`
+    }`
   }
   const matcher = value.match(/span\s+([0-9])/)
   if (matcher) {
-    return `${key.slice(5).replace('column', 'col')}-span-${
+    return `${important}${key.slice(5).replace('column', 'col')}-span-${
       matcher[1]
-    }${important}`
+    }`
   }
   if (trim(value, 'all') === '1/-1')
-    return `${key.slice(5).replace('column', 'col')}-span-full${important}`
-  return `${key.slice(5).replace('column', 'col')}-${value}${important}`
+    return `${important}${key.slice(5).replace('column', 'col')}-span-full`
+  return `${important}${key.slice(5).replace('column', 'col')}-${value}`
 }
