ae5ed0a2cd950d2bb6aef37f1b5c891001e88cb1
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-21 13:46:48 +0800

    chore(color): pass test

--- src/color.ts
+++ src/color.ts
@@ -3,5 +3,5 @@ import { getVal, transformImportant } from './utils'
 export function color(key: string, val: string) {
   const [value, important] = transformImportant(val)
 
-  return `text${getVal(value)}${important}`
+  return `${important}text${getVal(value)}`
 }
