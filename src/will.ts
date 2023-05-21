d9a04f2f3cf90b51248ba0d48dbb707ba1ee7852
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-21 19:23:41 +0800

    chore(will): pass test

--- src/will.ts
+++ src/will.ts
@@ -3,5 +3,5 @@ import { getFirstName, transformImportant } from './utils'
 export function will(key: string, val: string) {
   const [value, important] = transformImportant(val)
 
-  return `${key}-${getFirstName(value)}${important}`
+  return `${important}${key}-${getFirstName(value)}`
 }
