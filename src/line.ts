aa289bc5f8f249b5f2b10028eda6ac2628e93ea0
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-21 14:11:47 +0800

    chore(line-height): pass test

--- src/line.ts
+++ src/line.ts
@@ -3,5 +3,5 @@ import { getVal, transformImportant } from './utils'
 export function line(key: string, val: string) {
   const [value, important] = transformImportant(val)
 
-  return `lh${getVal(value)}${important}`
+  return `${important}lh${getVal(value)}`
 }
