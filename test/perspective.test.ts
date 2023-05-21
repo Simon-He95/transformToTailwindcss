80bb708ec5512f1212dd33f62e1f9c2385e5dd8d
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-21 14:21:45 +0800

    chore(perspective): pass test

--- test/perspective.test.ts
+++ test/perspective.test.ts
@@ -3,9 +3,7 @@ import { toTailwindcss } from '../src/toTailwindcss'
 
 describe('perspective', () => {
   it('px;', () => {
-    expect(toTailwindcss('perspective: 300px')).toBe(
-      'perspective-300px',
-    )
+    expect(toTailwindcss('perspective: 300px')).toBe('perspective-300px')
   })
 
   it('rem', () => {
@@ -13,6 +11,8 @@ describe('perspective', () => {
   })
 
   it('important', () => {
-    expect(toTailwindcss('perspective: 2em !important')).toBe('perspective-2em!')
+    expect(toTailwindcss('perspective: 2em !important')).toBe(
+      '!perspective-2em',
+    )
   })
 })
