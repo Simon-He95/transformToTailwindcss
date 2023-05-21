29e8192d421ae2913b3b08b53774e9227480f877
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-21 14:17:50 +0800

    chore(object): pass test

--- src/object.ts
+++ src/object.ts
@@ -4,6 +4,6 @@ export function object(key: string, val: string) {
   const [value, important] = transformImportant(val)
 
   if (key === 'object-position')
-    return `${getFirstName(key)}-${joinWithLine(value)}${important}`
-  return `${getFirstName(key)}-${value}${important}`
+    return `${important}${getFirstName(key)}-${joinWithLine(value)}`
+  return `${important}${getFirstName(key)}-${value}`
 }
