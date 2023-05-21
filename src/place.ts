24767158d52bca2d1ac8e261241b9c63bb6c268f
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-21 14:22:06 +0800

    chore(place): pass test

--- src/place.ts
+++ src/place.ts
@@ -3,5 +3,5 @@ import { getLastName, transformImportant } from './utils'
 export function place(key: string, val: string) {
   const [value, important] = transformImportant(val)
 
-  return `${key}-${getLastName(value)}${important}`
+  return `${important}${key}-${getLastName(value)}`
 }
