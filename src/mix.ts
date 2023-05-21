2193432ce7a42d9d8d67930c0dacb51d25c5f99f
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-21 14:16:40 +0800

    chore(mix): pass test

--- src/mix.ts
+++ src/mix.ts
@@ -3,5 +3,5 @@ import { transformImportant } from './utils'
 export function mix(key: string, val: string) {
   const [value, important] = transformImportant(val)
 
-  return `mix-blend-${value}${important}`
+  return `${important}mix-blend-${value}`
 }
