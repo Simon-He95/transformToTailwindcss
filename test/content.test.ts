715d189949009b4588b1dce79d8e39c6508ec8fa
Simon He <57086651+Simon-He95@users.noreply.github.com>
2023-05-21 13:57:02 +0800

    chore(content): pass test

--- test/content.test.ts
+++ test/content.test.ts
@@ -3,10 +3,10 @@ import { toTailwindcss } from '../src/toTailwindcss'
 
 describe('content', () => {
   it('content: none;', () => {
-    expect(toTailwindcss('content: none;')).toBe('content-none"')
+    expect(toTailwindcss('content: none;')).toBe('content-[none]')
   })
 
   it('content: "aa";', () => {
-    expect(toTailwindcss('content: "aa";')).toBe('content-["aa"]"')
+    expect(toTailwindcss('content: "aa";')).toBe('content-["aa"]')
   })
 })
